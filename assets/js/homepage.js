ca888d35b0ffd932f0af2ee3000843c7784a1b92
Fishdestroyer <jeffwhitner@gmail.com>
2022-01-26 14:01:38 -0600

    Updated index.html with language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -87,4 +87,18 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+      if(response.ok) {
+          response.json().then(function(data) {
+              displayRepos(data.items, language);
+          });
+      } else {
+          alert('Error: GitHub user not found');
+      }
+  });  
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
